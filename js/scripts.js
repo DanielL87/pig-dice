200b7143f5c9341daf0271865e399e4fd28e178b
Daniel Lira and Manasa Vesala <daniel@>
2018-10-31 16:16:45 -0700

    will complete 2 player functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,61 +4,124 @@ function rollDice() {
     return x;
 }
 
-
-function Player (turnScore, totalScore) {
-  this.turnScore = turnScore,
-  this.totalScore = totalScore
+function Player () {
+  this.turnScore = 0;
+  this.totalScore = 0;
 }
 
-var player1 = new Player(0,0);
-var player2 = new Player(0,0);
+var player1 = new Player();
+var player2 = new Player();
 var turnCount = 0;
+var score1 = 0;
+var score2 = 0;
+
 Player.prototype.play = function(currentScore){
-  if (turnCount % 2 == 0){
-    if(player1.totalScore>=100){
-      alert("Player1 won the game");
-    } else if (currentScore === 1){
-      player1.turnScore = 0;
+  // if (turnCount % 2 == 0){
+  //   if(player1.totalScore>=100){
+  //     alert("player1 won the game");
+  //   } else if (currentScore === 1){
+  //     player1.turnScore = 0;
+  //     turnCount++;
+  //     // console.log(turnCount);
+  //   }  else {
+  //   player1.turnScore = player1.turnScore + currentScore;
+  //   console.log(player1.turnScore);
+  //   }
+  //   score1 = player1.turnScore;
+  //   player1.totalScore = score1;
+  //
+  //   // this.totalScore = this.turnScore + this.totalScore;
+  // } else {
+  //   if(player2.totalScore>=100){
+  //     alert("player2 won the game");
+  //   }else if(currentScore === 1){
+  //     player2.turnScore = 0;
+  //     turnCount++;
+  //     // console.log(turnCount);
+  //   } else {
+  //   player2.turnScore = player2.turnScore + currentScore;
+  //   console.log(player2.turnScore);
+  //   }
+  //   var score2 = player2.turnScore;
+  //   player2.totalScore = score2;
+  //
+  // }
+
+
+    if (currentScore === 1){
+      this.turnScore = 0;
       turnCount++;
-      console.log(turnCount);
+      // console.log(turnCount);
     }  else {
-    player1.turnScore = player1.turnScore + currentScore;
-    player1.totalScore = player1.turnScore ;
+    this.turnScore = this.turnScore + currentScore;
     }
-  } else {
-    if(player2.totalScore>=100){
-      alert("Player2 won the game");
-    }else if(currentScore === 1){
-      player2.turnScore = 0;
-      turnCount++;
-      console.log(turnCount);
-    } else {
-    player2.turnScore = player2.turnScore + currentScore;
-    }
-    player2.totalScore = player2.turnScore + player2.totalScore;
-  }
+
 
 }
-
-
-
 
 // User Interface Logic --------
 $(document).ready(function() {
+  var passTotal1 = 0;
+  var passTotal2 = 0;
+
   $("#roll").click(function(event) {
     event.preventDefault();
     var randomNumber = rollDice();
     $("#rollnumber").text(randomNumber);
-    player2.play(randomNumber);
+
+
+    if(turnCount % 2 == 0){
+      player1.play(randomNumber);
+      // score1 = player1.turnScore;
+      // player1.totalScore = score1;
+
+      $("#turnScore").text(player1.turnScore);
+    } else {
+      player2.play(randomNumber);
+      // score2 = player2.turnScore;
+      // player2.totalScore = score2;
+      score2 = player2.turnScore;
+      $("#turnScore").text(player2.turnScore);
+    }
+
+    // $("#score1").text(player1.totalScore);
+    // $("#score2").text(player2.totalScore);
+
+
+  });
+
+  // $("#pass").click(function(event) {
+  //   event.preventDefault();
+  //   turnCount++;
+  //   $("#score1").text(score1);
+  //   $("#score2").text(score2);
+  // });
+  $("#pass").click(function(event) {
+    event.preventDefault();
+    // debugger;
 
 
 
-    $("#score1").text(player1.totalScore);
-    $("#score2").text(player2.totalScore);
+    if(turnCount % 2 == 0){
+      var passTotal1 = player1.turnScore;
 
+      player1.totalScore += passTotal1;
+      player1.turnScore = 0;
+      turnCount++;
+      $("#score1").text(player1.totalScore);
+    } else {
+      var passTotal2 = player2.turnScore;
 
+      player2.totalScore += passTotal2;
+      player2.turnScore = 0;
+      turnCount++;
+      $("#score2").text(player2.totalScore);
+    }
 
-
-
+    if(player1.totalScore>=100){
+      alert("player1 won the game");
+    } else if (player2.totalScore>=100) {
+      alert("player2 won the game");
+    }
   });
 });
