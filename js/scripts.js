1a8ac350782b1819e9aec96ea7da6b6be540db37
Daniel Lira and Manasa Vesala <daniel@>
2018-11-01 16:29:27 -0700

    adds images for rolled dice number

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function rollDice() {
     return x;
 }
 
+
 function Player () {
   this.turnScore = 0;
   this.totalScore = 0;
@@ -16,38 +17,6 @@ var score1 = 0;
 var score2 = 0;
 
 Player.prototype.play = function(currentScore){
-  // if (turnCount % 2 == 0){
-  //   if(player1.totalScore>=100){
-  //     alert("player1 won the game");
-  //   } else if (currentScore === 1){
-  //     player1.turnScore = 0;
-  //     turnCount++;
-  //     // console.log(turnCount);
-  //   }  else {
-  //   player1.turnScore = player1.turnScore + currentScore;
-  //   console.log(player1.turnScore);
-  //   }
-  //   score1 = player1.turnScore;
-  //   player1.totalScore = score1;
-  //
-  //   // this.totalScore = this.turnScore + this.totalScore;
-  // } else {
-  //   if(player2.totalScore>=100){
-  //     alert("player2 won the game");
-  //   }else if(currentScore === 1){
-  //     player2.turnScore = 0;
-  //     turnCount++;
-  //     // console.log(turnCount);
-  //   } else {
-  //   player2.turnScore = player2.turnScore + currentScore;
-  //   console.log(player2.turnScore);
-  //   }
-  //   var score2 = player2.turnScore;
-  //   player2.totalScore = score2;
-  //
-  // }
-
-
     if (currentScore === 1){
       this.turnScore = 0;
       turnCount++;
@@ -63,11 +32,10 @@ Player.prototype.play = function(currentScore){
 $(document).ready(function() {
   var passTotal1 = 0;
   var passTotal2 = 0;
-
   $("#roll").click(function(event) {
     event.preventDefault();
     var randomNumber = rollDice();
-    $("#rollnumber").text(randomNumber);
+    // $("#rollnumber").text(randomNumber);
 
 
     if(turnCount % 2 == 0){
@@ -78,29 +46,13 @@ $(document).ready(function() {
       $("#turnScore").text(player1.turnScore);
     } else {
       player2.play(randomNumber);
-      // score2 = player2.turnScore;
-      // player2.totalScore = score2;
       score2 = player2.turnScore;
       $("#turnScore").text(player2.turnScore);
     }
-
-    // $("#score1").text(player1.totalScore);
-    // $("#score2").text(player2.totalScore);
-
-
+    $("#rolldice").html("<img src='img/" + randomNumber + ".png'>");
   });
-
-  // $("#pass").click(function(event) {
-  //   event.preventDefault();
-  //   turnCount++;
-  //   $("#score1").text(score1);
-  //   $("#score2").text(score2);
-  // });
   $("#pass").click(function(event) {
     event.preventDefault();
-    // debugger;
-
-
 
     if(turnCount % 2 == 0){
       var passTotal1 = player1.turnScore;
